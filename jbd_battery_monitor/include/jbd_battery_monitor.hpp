7e51e8434ee7af08175e57968cd33a71d6edc231
AiriY <airi.yokochi@g.softbank.co.jp>
2021-01-18 15:21:53 +0900

    ＢＭＳから電池の情報を取得してＰｕｂするソースコードの追加

---
+++ jbd_battery_monitor/include/jbd_battery_monitor.hpp
@@ -0,0 +1,28 @@
+#ifndef JBD_BATTERY_MONITOR
+#define JBD_BATTERY_MONITOR
+
+#include <cstdlib>
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "serial_communication.hpp"
+
+
+enum Params {
+  BATTERY_REMAINING,
+  BATTERY_VOLTAGE,
+  CHARGE_CURRENT
+};
+std::vector<double> battery_status_(3,0.0);
+
+#endif //JBD_BATTERY_MONITOR

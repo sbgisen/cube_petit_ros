81b237397c41f038d64ba426ce9a13bdeefcd1a3
AYokochi <airi.yokochi@g.softbank.co.jp>
2023-07-25 11:36:32 +0900

    add custom battery remain

--- jbd_battery_monitor/src/jbd_battery_monitor.cpp
+++ jbd_battery_monitor/src/jbd_battery_monitor.cpp
@@ -9,7 +9,7 @@
 ////////////////////////////////////////////
 
 /*
-Publish: 
+Publish:
 
 /battery_simulation/battery_remaining
 /battery_simulation/battery_voltage
@@ -26,18 +26,29 @@ Non Publish
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "jbd_battery_monitor");
-  
+
   ros::NodeHandle nh("~");
   ros::Publisher battery_remaining_pub = nh.advertise<std_msgs::Float64>("/gazebo_battery_monitor/battery_remaining", 10);
   ros::Publisher battery_voltage_pub = nh.advertise<std_msgs::Float64>("//gazebo_battery_monitor/battery_voltage", 10);
   ros::Publisher charge_current_pub = nh.advertise<std_msgs::Float64>("/gazebo_battery_monitor/charge_current", 10);
-  
+
   std_msgs::Float64 battery_remaining;
   std_msgs::Float64 battery_voltage;
   std_msgs::Float64 charge_current;
 
   bool once_flag = true;
+  bool custom_voltage_remain = true;
   nh.getParam("/jbd_battery_monitor/once_flag", once_flag);  //launchから起動すること
+  nh.getParam("/jbd_battery_monitor/custom_voltage_remain", custom_voltage_remain);
+
+  // Batteryの MAX & MIN 電圧からバッテリの残量を算出する(BMSからのbattery_remainingが微妙なので)
+  double max_voltage = 13.80;
+  double min_voltage = 11.20;
+  nh.getParam("/jbd_battery_monitor/max_voltage", max_voltage);
+  nh.getParam("/jbd_battery_monitor/min_voltage", min_voltage);
+
+
+
   ROS_INFO("once_flag %d", once_flag);
 
   Serial_Communication serial_comminication;
@@ -45,9 +56,9 @@ int main(int argc, char **argv){
     ROS_ERROR("Could Not Open Serial");
     return 1;
   }
-  
+
   ros::Rate loop_rate(20);
-  serial_comminication.updateBatteryStatus(battery_status_); 
+  serial_comminication.updateBatteryStatus(battery_status_);
 
   int count = 0;
   int loop_time = 0;
@@ -58,14 +69,27 @@ int main(int argc, char **argv){
       //continue;
     }
 
-  // 値を読み込むのは1Hz
-    serial_comminication.updateBatteryStatus(battery_status_);    
+    // 値を読み込むのは1Hz
+    serial_comminication.updateBatteryStatus(battery_status_);
     battery_remaining.data = battery_status_[BATTERY_REMAINING];
     battery_voltage.data = battery_status_[BATTERY_VOLTAGE];
+
+    // Batteryの MAX & MIN 電圧からバッテリの残量を算出する(現在BMSからのbattery_remainingが微妙なので)
+    if( custom_voltage_remain ){
+        double battery_remaining_num = 100 * (battery_status_[BATTERY_VOLTAGE] - min_voltage) / (max_voltage - min_voltage);
+        if (battery_remaining_num > 100){
+            battery_remaining_num = 100;
+        }
+        battery_remaining.data = battery_remaining_num;
+
+    }
+
     charge_current.data = battery_status_[CHARGE_CURRENT];
     battery_remaining_pub.publish(battery_remaining);
     battery_voltage_pub.publish(battery_voltage);
     charge_current_pub.publish(charge_current);
+
+
 
     ros::spinOnce();
     loop_rate.sleep();

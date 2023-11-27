5bed487db4e9ee01825b07784f36e322ef9da67f
AYokochi <airi.yokochi@g.softbank.co.jp>
2023-11-27 16:31:32 +0900

    fix jbd battery node

--- jbd_battery_monitor/include/serial_communication.hpp
+++ jbd_battery_monitor/include/serial_communication.hpp
@@ -20,56 +20,53 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <bitset>
-#include <errno.h> // Error integer and strerror() function
+#include <errno.h>  // Error integer and strerror() function
 
-#define SERIAL_PORT "/dev/ttyUSB0"
-#define BAUD_RATE    B9600                // RS232C通信ボーレート
-#define BUFF_SIZE    4096                 // 適当
+#define SERIAL_PORT "/dev/ttyJBDBM"
+#define BAUD_RATE B9600  // RS232C通信ボーレート
+#define BUFF_SIZE 4096   // 適当
 
 class Serial_Parse;
 
-class Serial_Communication{
-
+class SerialCommunication
+{
 private:
-  bool data_start_flag;
-  bool data_end_flag;
-  int data_count;
+  bool data_start_flag_;
+  bool data_end_flag_;
+  int data_count_;
 
-  int fd;
-  uint8_t buf;                        //2byte
-  std::vector<uint8_t> data_vector;   //0xDDから始まって0x77で終わるデータを格納する
-  double battery_remaining;
-  double battery_voltage;
-  double charge_current;
+  int fd_;
+  uint8_t buf_;                       // 2byte
+  std::vector<uint8_t> data_vector_;  // 0xDDから始まって0x77で終わるデータを格納する
+  double battery_remaining_;
+  double battery_voltage_;
+  double charge_current_;
 
-  enum Params {
+  enum Params
+  {
     BATTERY_REMAINING,
     BATTERY_VOLTAGE,
     CHARGE_CURRENT
   };
 
 public:
-  int is_usb_connected;   // 0: no connection 1: connected  2: reconnecting
+  int is_usb_connected_;  // 0: no connection 1: connected  2: reconnecting
 
-  Serial_Communication();
-  // Serial_Communication(Serial_Communication* serial_communication);
-  ~Serial_Communication();
-  int serial_initialize();
-  int serial_close();
-  int serial_open();
-  int serial_ping();
-  int serial_reconnect();
-  int serial_send(uint8_t *data);
-  int serial_read();
+  SerialCommunication();
+  // SerialCommunication(SerialCommunication* serial_communication);
+  ~SerialCommunication();
+  int serialInitialize();
+  int serialClose();
+  int serialOpen();
+  int serialPing();
+  int serialReconnect();
+  int serialSend(uint8_t* data);
+  int serialRead();
   void updateBatteryStatus(std::vector<double>&);
   int requsetBatteryStatus();
   int requestEachBatteryVoltage();
-  void requset_loop();
-  int serial_parse(std::vector<uint8_t>& data);
-
-
+  void requsetLoop();
+  int serialParse(std::vector<uint8_t>& data);
 };
 
-#endif //SERIAL_COMMUNICATION
-
-
+#endif  // SERIAL_COMMUNICATION

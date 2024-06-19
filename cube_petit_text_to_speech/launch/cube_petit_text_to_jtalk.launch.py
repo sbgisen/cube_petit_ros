83f71d44da700978b67d4bc2d8a692575c4f76ba
AYokochi <airi.yokochi@g.softbank.co.jp>
2024-06-19 15:47:36 +0900

    add teleop and talk to bringup launch

--- cube_petit_text_to_speech/launch/cube_petit_text_to_jtalk.launch.py
+++ cube_petit_text_to_speech/launch/cube_petit_text_to_jtalk.launch.py
@@ -15,34 +15,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Launch file."""
-from launch_ros.actions import Node
+# import os
 
-from launch import LaunchDescription
-
-from ament_index_python.packages import get_package_share_directory
+# from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 # from launch.actions import GroupAction
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
+# from launch.actions import DeclareLaunchArgument
+# from launch.actions import IncludeLaunchDescription
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
 
 def generate_launch_description() -> LaunchDescription:
     """Generate launch description."""
 
-    teleop_twist_joy_dir = get_package_share_directory('teleop_twist_joy')
-    cube_teleop_dir = get_package_share_directory('cube_petit_bringup')
-    joy_dev = '/dev/input/js0'
-    config_filepath = os.path.join(
-        cube_teleop_dir, 'config', 'ps4.config.yaml'
-    )
-    teleop_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(teleop_twist_joy_dir, 'launch', 'teleop-launch.py')),
-        launch_arguments={
-            'joy_dev': joy_dev,
-            'config_filepath': config_filepath
-        }.items()
-    )
+    # teleop_twist_joy_dir = get_package_share_directory('teleop_twist_joy')
+    # cube_teleop_dir = get_package_share_directory('cube_petit_bringup')
+    # joy_dev = '/dev/input/js0'
+    # config_filepath = os.path.join(
+    #     cube_teleop_dir, 'config', 'ps4.config.yaml'
+    # )
+    # teleop_include = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(os.path.join(teleop_twist_joy_dir, 'launch', 'teleop-launch.py')),
+    #     launch_arguments={
+    #         'joy_dev': joy_dev,
+    #         'config_filepath': config_filepath
+    #     }.items()
+    # )
 
     return LaunchDescription([
         Node(
@@ -61,5 +60,5 @@ def generate_launch_description() -> LaunchDescription:
             name='depthai_hand_tracker',
             output='screen'
         ),
-        teleop_include
+        # teleop_include
     ])

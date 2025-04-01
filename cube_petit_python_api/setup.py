f13cf341be0e1b4400ebb31623779916f36a7d4c
Airi Yokochi <airi.yokochi@g.softbank.co.jp>
2025-04-01 13:13:07 +0900

    fix setup py

--- cube_petit_python_api/setup.py
+++ cube_petit_python_api/setup.py
@@ -40,6 +40,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/config', glob.glob('./config/*.txt')),
         (f'share/{package_name}/launch', glob.glob('./launch/*.launch.py')),
     ],
     maintainer='gisen',
